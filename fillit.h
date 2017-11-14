cc3c02993cf912c3d6a6e12d3a95d5c36912d976
Mykola VUKOLOV <mvukolov@e1r12p1.unit.ua>
2017-11-14 17:13:47 +0200

    now it stores every tetris in a list

--- fillit.h
+++ fillit.h
@@ -7,8 +7,10 @@
 # include <stdlib.h>
 # include <unistd.h>
 # include <stdio.h>
+# include "libft.h"
 # define BUFFSIZE 21
 
 t_list	*get_tetris_list(char const *filename);
+void	file_input_error();
 
 #endif
--- get_tetris_list.c
+++ get_tetris_list.c
@@ -1,26 +1,38 @@
-#include "libft.h"
+
 #include "fillit.h"
 
 /*This function takes a file and reads first BUFFSIZE-1 chars
 and returns a pointer with the result of reading
 BUt to be cool it actually should skip and \n chars first*/
+static void printlist(t_list *list)
+{
+	ft_putendl(list->content);
+}
+
 static char	*get_current_tetris(int fd)
 {
 	int		bytes_read;
 	char	*buf;
 
 	buf = ft_strnew(BUFFSIZE);
-	if (bytes_read = read(fd, buf, BUFFSIZE - 1))
+	if ((bytes_read = read(fd, buf, BUFFSIZE - 1)))
 	{
 		buf[bytes_read] = 0;
 		if (bytes_read < BUFFSIZE - 1)
 		{
 			free(buf);
+			file_input_error();
 			return (NULL);
 		}
 		return (buf);
 	}
 	return (NULL);
+}
+
+/* Function gives each tetris an index */
+char 	validate_tetris(char *tetris)
+{
+	
 }
 
 t_list		*get_tetris_list(char const *filename)
@@ -29,6 +41,7 @@ t_list		*get_tetris_list(char const *filename)
 	char	*curr_tetris;
 	int		fd;
 
+	tetris_list = NULL;
 	/*actually I should not return null here, 
 	instead I should print out the mistake and exit!!!!*/
 	if (!(fd = open(filename, O_RDONLY))) 
@@ -37,11 +50,12 @@ t_list		*get_tetris_list(char const *filename)
 	{
 		if (curr_tetris)
 		{
-			ft_putendl(curr_tetris);
+			ft_lstadd(&tetris_list, ft_lstnew(curr_tetris, ft_strlen(curr_tetris)));
 			read(fd, curr_tetris, 1);
 			free(curr_tetris);
 		}
 	}
+	ft_lstiter(tetris_list, &printlist);
 	close(fd);
 	return (tetris_list);
 }
